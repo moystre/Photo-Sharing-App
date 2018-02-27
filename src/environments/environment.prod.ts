a38bc0261184e0b04b09446f325b6d3e4111f4de
moystre <moystre@gmail.com>
2018-02-27 01:42:13 +0100

    firebase configuration
    environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDX8HnrVccyZTsqWK04Xg1sbKNgVJ0w_10',
+    authDomain: 'photo-sharing-app-b1663.firebaseapp.com',
+    databaseURL: 'https://photo-sharing-app-b1663.firebaseio.com',
+    projectId: 'photo-sharing-app-b1663',
+    storageBucket: 'photo-sharing-app-b1663.appspot.com',
+    messagingSenderId: '361952141915'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDX8HnrVccyZTsqWK04Xg1sbKNgVJ0w_10',
+    authDomain: 'photo-sharing-app-b1663.firebaseapp.com',
+    databaseURL: 'https://photo-sharing-app-b1663.firebaseio.com',
+    projectId: 'photo-sharing-app-b1663',
+    storageBucket: 'photo-sharing-app-b1663.appspot.com',
+    messagingSenderId: '361952141915'
+  }
 };
