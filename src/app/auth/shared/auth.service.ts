d109409a3921f5abe01ec78a979dc7b31d55108e
moystre <moystre@gmail.com>
2018-03-03 21:38:01 +0100

    logout() & isAuthenticated()

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,5 +1,7 @@
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/operator/map';
 
 @Injectable()
 export class AuthService {
@@ -8,6 +10,17 @@ export class AuthService {
 
   login(email: string, password: string): Promise<any> {
     return this.fireAuth.auth.signInAndRetrieveDataWithEmailAndPassword(email, password)
+  }
+
+  logout(): Promise<any> {
+    return this.fireAuth.auth.signOut();
+  }
+
+  isAuthenticated(): Observable<boolean> {
+    return this.fireAuth.authState
+    .map(authState => {
+      return authState !== null;
+    });
   }
 
 }
