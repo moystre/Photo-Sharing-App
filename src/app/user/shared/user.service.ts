a4d24666f904a16c28b1b6134f3f12a0abf9bbaa
moystre <moystre@gmail.com>
2018-03-11 17:37:52 +0100

    getUser() in user.service

--- src/app/user/shared/user.service.ts
+++ src/app/user/shared/user.service.ts
@@ -1,8 +1,10 @@
+import { element } from 'protractor';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from './../../auth/shared/auth.service';
 import { Injectable } from '@angular/core';
 import { User } from './user';
 import { AngularFirestore } from 'angularfire2/firestore';
+import 'rxjs/add/operator/first';
 
 @Injectable()
 export class UserService {
@@ -11,7 +13,18 @@ export class UserService {
               private angularFireStore: AngularFirestore) { }
 
   getUser(): Observable<User> {
-    return this.authService.getAuthUser();
+    // 1. Get the AuthUser
+    // 2. Get the DBUser - with SwitchMap we prevent function from 3. until we have 1. & 2.
+    // 3. Merge AuthUser & DBUser - using Map to map to convert result of merging nad returning an Obs
+    return this.authService.getAuthUser().first()
+      .switchMap(authUser => {
+        return this.angularFireStore.doc<User>('users/' + authUser.uid).valueChanges().first()
+        .map(dbUser => {
+          dbUser.uid = authUser.uid;
+          dbUser.email = authUser.email;
+          return dbUser;
+        });
+      });
   }
 
   update(user: User): Promise<any> {
