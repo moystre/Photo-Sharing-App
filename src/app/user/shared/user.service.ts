d9e50ac98a6a5f8419f6f0501f90e0d38b5208b3
moystre <moystre@gmail.com>
2018-03-11 15:30:28 +0100

    update User in user.service

--- src/app/user/shared/user.service.ts
+++ src/app/user/shared/user.service.ts
@@ -2,13 +2,19 @@ import { Observable } from 'rxjs/Observable';
 import { AuthService } from './../../auth/shared/auth.service';
 import { Injectable } from '@angular/core';
 import { User } from './user';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Injectable()
 export class UserService {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,
+              private angularFireStore: AngularFirestore) { }
 
   getUser(): Observable<User> {
     return this.authService.getAuthUser();
+  }
+
+  update(user: User): Promise<any> {
+    return this.angularFireStore.doc('users/' + user.uid).set(user);
   }
 }
