984560903b33e7d43643f8f59b06909d8ffb8bec
moystre <moystre@gmail.com>
2018-03-19 11:18:37 +0100

    AngularFireStorageModule* fix for file-system

--- src/app/file-system/file-system.module.ts
+++ src/app/file-system/file-system.module.ts
@@ -1,12 +1,12 @@
 import { FileService } from './file.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
 
 @NgModule({
   imports: [
     CommonModule,
-    AngularFirestoreModule
+    AngularFireStorageModule
   ],
   declarations: [],
   providers: [FileService]
