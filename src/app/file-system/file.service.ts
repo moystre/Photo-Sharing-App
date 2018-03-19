e2e5241fbfa61517595b79f04cd429e6f9ccbdbd
moystre <moystre@gmail.com>
2018-03-19 10:34:32 +0100

    Upload in File service with upload-task

--- src/app/file-system/file.service.ts
+++ src/app/file-system/file.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 import { AngularFireStorage } from 'angularfire2/storage';
+import { UploadTask } from './upload-task';
 
 @Injectable()
 export class FileService {
 
   constructor(private fireStorage: AngularFireStorage) { }
 
+  upload(path: string, file: File): UploadTask {
+    const task = this.fireStorage.upload(path, file);
+    return {
+      downloadUrl: task.downloadURL()
+    };
+  }
 }
