962eb355556d76e78d2bc801b03b5c683201bd75
moystre <moystre@gmail.com>
2018-04-01 14:35:06 +0200

    adding file-details & file-system-container

--- src/app/file-system/file-system.module.ts
+++ src/app/file-system/file-system.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FileDetailsComponent } from './file-details/file-details.component';
+import { FileSystemContainerComponent } from './file-system-container/file-system-container.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [],
-  providers: []
+  declarations: [FileDetailsComponent, FileSystemContainerComponent],
+  providers: [],
+  exports: [FileSystemContainerComponent]
 })
 export class FileSystemModule { }
