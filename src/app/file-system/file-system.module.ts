05086f9090de0384e763d01a7e402ca06b7cac4e
moystre <moystre@gmail.com>
2018-04-01 16:11:18 +0200

    UI items in file-details

--- src/app/file-system/file-system.module.ts
+++ src/app/file-system/file-system.module.ts
@@ -1,11 +1,19 @@
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FileDetailsComponent } from './file-details/file-details.component';
 import { FileSystemContainerComponent } from './file-system-container/file-system-container.component';
+import { MatListModule, MatButtonModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule
   ],
   declarations: [FileDetailsComponent, FileSystemContainerComponent],
   providers: [],
