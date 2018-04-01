d7b4c80911e5fa9158c7d3f8d0a11be4d8493a0a
moystre <moystre@gmail.com>
2018-04-01 14:56:43 +0200

    displaying file-system in album-list tryout

--- src/app/albums/albums.module.ts
+++ src/app/albums/albums.module.ts
@@ -1,10 +1,11 @@
+import { FileSystemModule } from './../file-system/file-system.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlbumsListComponent } from './albums-list/albums-list.component';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, FileSystemModule],
   declarations: [AlbumsListComponent]
 })
 export class AlbumsModule {}
